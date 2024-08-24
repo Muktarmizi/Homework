db757e57c54524d4fb3a2238d15b7b6949edb286
Muktarmizi <Mizimuktar0@gmail.com>
2024-08-24 19:59:18 -0400

    update

---
+++ src/hw4Constructor/Student.java
@@ -0,0 +1,38 @@
+package hw4Constructor;
+
+public class Student {
+
+//Declare some variable --> name, id, gender, grade (as float), isProgrammer(as boolean).
+	public String name;
+	public int id;
+	public char gender;
+	public float grade;
+	public boolean isProgrammer;
+	// default constructor
+	public Student () {
+		System.out.println("This is all about Student");
+		}
+
+	public Student(String name) {
+		this.name = name;
+		System.out.println("NEW STUDENT NAME"+name);
+		}
+
+	public Student(String name, char gender, int id, float grade, boolean isProgrammer) {
+		this.name = name;
+		this.gender = gender;
+		this.id = id;
+		this.grade = grade;
+		this.isProgrammer = isProgrammer;
+		System.out.println("NEW STUDENT NAME:  "+ name +", STUDENT GENDER:  "+ gender +", STUDENT ID:   "+ id  +",  STUDENT GRADE:  "+ grade  +", JAVA PROGRAMMER:  "+ isProgrammer);
+		}
+	
+	
+		
+	
+	
+	
+	
+	
+	
+}
---
+++ src/hw4Constructor/StudentTest.java
@@ -0,0 +1,15 @@
+package hw4Constructor;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+	Student student=new Student();
+
+	System.out.println("<<<<<<<<<<<<<>>>>>>>>>");
+	
+	Student profile=new Student("Muktar mizi",'M',12345,3.5000f,true );
+	
+	
+	}
+
+}
