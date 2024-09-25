b50899d43b8da0813310e03c7e82de9c3084628c
Muktarmizi <Mizimuktar0@gmail.com>
2024-09-24 21:01:46 -0400

    update

--- src/hw15IfElseCondition/Voter.java
+++ src/hw15IfElseCondition/Voter.java
@@ -4,21 +4,14 @@ public class Voter {
 
 	public static void main(String[] args) {
 		int age1 = 19;
-		int voterAge = 18;
 
-		if (age1 == voterAge) {
-			System.out.println(age1 + " is equal to " + voterAge);
+		if (age1 == 18) {
 			System.out.println("I am a Voter");
-		}
-		else if (age1 < voterAge) {
-			System.out.println(age1 + " is smaller than " + voterAge);
+		} else if (age1 < 18) {
 			System.out.println("I am not a Voter");
-		} 
-		else if (age1 > voterAge) {
-			System.out.println(age1 + " is greater than " + voterAge);
+		} else if (age1 > 18) {
 			System.out.println("I am a Voter from age 18");
-		} 
-		else {
+		} else {
 			System.out.println("Please add a valid age");
 
 		}
