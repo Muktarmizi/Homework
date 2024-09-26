e9fd6c684f29e9b097a8a71d74b26bed545906e6
Muktarmizi <Mizimuktar0@gmail.com>
2024-09-25 22:32:24 -0400

    update

--- src/hw16IfElseCondition/Temperature.java
+++ src/hw16IfElseCondition/Temperature.java
@@ -5,33 +5,24 @@ import java.util.Scanner;
 public class Temperature {
 	public static void main(String[] args) {
 		System.out.println("Today's Weather");
-		Scanner scanner = new Scanner(System.in);
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Please enter today's temperature: ");
 		int todaysTemperature = scanner.nextInt();
-		
+
 		if (todaysTemperature < 32) {
-			System.out.println("\" is smaller than - 32\" ");
 			System.out.println("Freezing");
-		} 
-		else if (todaysTemperature < 55) {
-			System.out.println("\" is smaller than - 55 \" ");
+		} else if (todaysTemperature < 55) {
 			System.out.println("Pleasant");
-		} 
-		else if (todaysTemperature < 73) {
-			System.out.println("\" is smaller than - 73 \" ");
+		} else if (todaysTemperature < 73) {
 			System.out.println("Getting Warmer");
-		} 
-		else if (todaysTemperature < 101) { // today's weather is 75.
-			System.out.println("\" is smaller than - 101\" ");
+		} else if (todaysTemperature < 101) { // today's weather is 75.
 			System.out.println("Hot");
-		} 
-		else if (todaysTemperature >= 101) {
-			System.out.println("\" is greater than or equal to - 101\"");
+		} else if (todaysTemperature >= 101) {
 			System.out.println("Very Hot");
-		} 
-		else {
+		} else {
 			System.out.println("Please put a valid temperature");
 		}
 		scanner.close();
-}
+	}
 }
